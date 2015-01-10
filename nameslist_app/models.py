9d44aaadd26b2a7e3e42342b249c75c8f55afdfb
dfallon <dfallon@imagineyourweb.com>
2015-01-10 07:07:06 -0600

    completed models as specified (some name changes, but minor) created migrations

--- nameslist_app/models.py
+++ nameslist_app/models.py
@@ -1,17 +1,55 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Prospective(models.Model):
-    pass
+    id = models.AutoField(primary_key=True)
+
 class Name(models.Model):
-    pass
+    prospective_id = models.ForeignKey(Prospective)
+    user_id = models.ForeignKey(User)
+    name = models.CharField(max_length=256)
+    correct = models.BooleanField(default=False)
+    class Meta:
+        unique_together = ('prospective_id','user_id',)
+
 class Photo(models.Model):
-    pass
-class Fact(models.Model):
-    pass
+    prospective_id = models.ForeignKey(Prospective)
+    user_id = models.ForeignKey(User)
+    url = models.TextField()
+    primary = models.BooleanField(default=False)
+    class Meta:
+        unique_together = ('prospective_id','user_id',)
+
 class Fact_Type(models.Model):
-    pass
-class Opinion(models.Model):
-    pass
+    sortorder = models.PositiveIntegerField(unique=True)
+    public = models.BooleanField(default=False)
+    name = models.CharField(max_length=256)
+    TEXT = 0
+    BLOB = 1
+    PROMPT_TYPE_CHOICES = (
+        (0,'text'),
+        (1,'blob')
+    )
+    prompt_type = models.SmallIntegerField(choices=PROMPT_TYPE_CHOICES)
+    question = models.TextField()
+
+class Fact(models.Model):
+    fact_type_id = models.ForeignKey(Fact_Type)
+    user_id = models.ForeignKey(User, null=True)
+    prospective_id = models.ForeignKey(Prospective)
+    fact = models.CharField(max_length=256)
+    class Meta:
+        unique_together = ('fact_type_id','user_id','prospective_id',)
+
 class Opinion_Prompt(models.Model):
-    pass
+    sortorder = models.PositiveIntegerField(unique=True)
+    prompt_text = models.TextField()
+
+class Opinion(models.Model):
+    opinion_prompt_id = models.ForeignKey(Opinion_Prompt)
+    user_id = models.ForeignKey(User)
+    prospective_id = models.ForeignKey(Prospective)
+    opinion = models.TextField()
+    class Meta:
+        unique_together = ('opinion_prompt_id','user_id','prospective_id',)
