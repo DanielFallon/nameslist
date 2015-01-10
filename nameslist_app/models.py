b3f205892ee7484e0f506bfc1ccf340202b111ec
dfallon <dfallon@imagineyourweb.com>
2015-01-10 06:44:06 -0600

    created model names

--- nameslist_app/models.py
+++ nameslist_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Prospective(models.Model):
+    pass
+class Name(models.Model):
+    pass
+class Photo(models.Model):
+    pass
+class Fact(models.Model):
+    pass
+class Fact_Type(models.Model):
+    pass
+class Opinion(models.Model):
+    pass
+class Opinion_Prompt(models.Model):
+    pass
