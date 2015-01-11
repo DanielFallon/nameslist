b3c3868ac6b952b37fe44a7f6bcaac009eeb5bb0
dfallon <dfallon@imagineyourweb.com>
2015-01-10 19:02:14 -0600

    added some admin.py registrations as examples

--- nameslist_app/admin.py
+++ nameslist_app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from nameslist_app.models import Prospective,Name,Photo
 
 # Register your models here.
+class NameInline(admin.TabularInline):
+    model = Name
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+class ProspectiveAdmin(admin.ModelAdmin):
+    inlines = [NameInline,PhotoInline]
+admin.site.register(Prospective,ProspectiveAdmin)
