1cbbda689ca96e1ace3d510360f534f954f33fb3
Peter Chinetti <peter@chinetti.me>
2015-01-11 02:26:33 -0600

    Added the url regex for the index page, fix some pep8 prettyness, and built an index view.

--- nameslist_app/views.py
+++ nameslist_app/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render_to_response
+
+from nameslist_app import models
+
+
+def index(request):
+    form_elements = models.Fact_Type.objects.filter(public=True)
+    return render(request, 'index.html', {'form_elements': form_elements})
+
